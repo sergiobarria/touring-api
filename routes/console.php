2a262d386f6f1fc3d5e4a288e48cb6575e819b28
sergiobarria <sbarria.dev@gmail.com>
2024-10-13 15:50:30 -0500

    Move health check config from Kernel.php into console.php

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Spatie\Health\Commands\RunHealthChecksCommand;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+if (app()->environment('production')) {
+    Schedule::command(RunHealthChecksCommand::class)->everyMinute();
+} else {
+    Schedule::command(RunHealthChecksCommand::class)->hourly();
+}
